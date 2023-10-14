e44a06e84859613175957cf3a09e4fa0f74f0bd4
codenamewei <codenamewei@gmail.com>
2023-10-15 00:11:35 +0900

    wip

--- fastapi/event_handling/part0_event_handling_hello_world/server.py
+++ fastapi/event_handling/part0_event_handling_hello_world/server.py
@@ -11,12 +11,19 @@ async def startup_event():
     items["foo"] = {"name": mod1.add_one(0)}
     items["bar"] = {"name": mod1.add_one(1)}
 
+
+@app.on_event("shutdown")
+async def shutdown_event():
+
+    with open("log.txt", "a") as f:
+        f.write("Application close\n")
+
+
 @app.get("/items/{item_id}")
 async def read_items(item_id: str):
-    
-    
+    return items[item_id]
 
 @app.get("/")
 async def welcome():
 
-    return dict(message = "Welcome to part 0: native build!")
+    return dict(message = "Welcome to part 1: event handling!")
